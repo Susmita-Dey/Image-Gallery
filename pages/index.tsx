69c0a0f5b65c8792a6212b17d9266e1d3843951c
Susmita-Dey <susmitadey475@gmail.com>
2023-02-20 15:57:26 +0530

    initial files

---
+++ pages/index.tsx
@@ -0,0 +1,79 @@
+import Image from "next/legacy/image";
+import { useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.SUPABASE_SERVICE_ROLE_KEY || ""
+);
+
+export async function getStaticProps() {
+  // await supabaseAdmin.from("images").insert([
+  //   {
+  //     name: "Pedro Duarte",
+  //     href: "https://twitter.com/peduarte/status/1463897468383412231",
+  //     username: "@peduarte",
+  //     imageSrc: "https://pbs.twimg.com/media/FFDOtLkWYAsWjTM?format=jpg",
+  //   },
+  // ]);
+
+  const { data } = await supabaseAdmin.from("images").select("*").order("id");
+  return {
+    props: {
+      images: data,
+    },
+  };
+}
+
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+type Image = {
+  id: number;
+  href: string;
+  imageSrc: string;
+  name: string;
+  username: string;
+};
+
+export default function Gallery({ images }: { images: Image[] }) {
+  return (
+    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <h1 className="mt-4 mb-12 text-center text-5xl font-bold underline decoration-slice decoration-gray-500">
+        Image Gallery
+      </h1>
+      <div className="grid grdi-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {images.map((image) => (
+          <BlurImage key={image.id} image={image} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function BlurImage({ image }: { image: Image[] }) {
+  const [isLoading, setLoading] = useState(true);
+  return (
+    <a href={image.href} className="group">
+      <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
+        <Image
+          alt=""
+          src={image.imageSrc}
+          className={cn(
+            "group-hover:opacity-75",
+            isLoading
+              ? "grayscale blur-2xl scale-110"
+              : "grayscale-0 blur-0 scale-100"
+          )}
+          onLoadingComplete={() => setLoading(false)}
+          layout="fill"
+          objectFit="cover"
+          priority
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
+    </a>
+  );
+}
